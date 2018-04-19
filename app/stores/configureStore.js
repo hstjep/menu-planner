80f885e2105717547cca501e1c08859297c76654
Helena Stjepanovic <helena.stjepanovic3@gmail.com>
2018-04-19 22:27:10 +0200

    used redux dev tools extension instead of redux logger

--- app/stores/configureStore.js
+++ app/stores/configureStore.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
-import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(
+	reducer, composeWithDevTools(
+	applyMiddleware(thunk)));
 export default store;
